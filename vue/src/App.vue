84aec59e59d74aa2e78bac96d47704f25451591c
Malik Smith <m.smith1017.whhs@gmail.com>
2024-04-05 14:44:53 -0400

    added additional navbar stlying and added logo image

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="capstone-app">
     <header id="header">
-      <nav id="nav" class="navbar">
-        <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
-        <router-link class="nav-link" :to="{ name: 'register' }" v-if="$store.state.token == ''">Register</router-link>
-        <router-link class="nav-link" :to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
-        <router-link class="nav-link" :to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <nav id="nav" class="navbar justify-content-between">
+        <img class="navbar-brand" src="@/assets/nav-logo.png" alt="woofr">
+        <div class="router-links">
+          <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
+          <router-link class="nav-link" :to="{ name: 'register' }" v-if="$store.state.token == ''">Register</router-link>
+          <router-link class="nav-link" :to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
+          <router-link class="nav-link" :to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+        </div>
       </nav>
     </header>
     <router-view />
   </div>
 </template>
 
-<style>
-@import url('https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=McLaren&display=swap');
+<style scoped>
+img {
+  height: 100%;
+  width: auto;
+  margin-left: 20px;
+}
+
+
+.navbar {
+  height: 100%;
+}
 </style>
