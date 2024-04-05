209c5a71b89c8965e61a41292c538e36feec95b0
Kae Bonaguro <k.n.bonaguro@gmail.com>
2024-04-05 13:07:51 -0400

    Configured index.js file to import custom stylesheet

--- vue/src/main.js
+++ vue/src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import axios from 'axios'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/assets/styles.css'
 
 /* sets the base url for server API communication with axios */
 axios.defaults.baseURL = import.meta.env.VITE_REMOTE_API;
