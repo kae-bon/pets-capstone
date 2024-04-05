c319c1805ceb1fb248dd549636c2225ab2a811f7
Malik Smith <m.smith1017.whhs@gmail.com>
2024-04-05 14:02:42 -0400

    added navbar styling with default functionality

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="capstone-app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
+    <header id="header">
+      <nav id="nav" class="navbar">
+        <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
+        <router-link class="nav-link" :to="{ name: 'register' }" v-if="$store.state.token == ''">Register</router-link>
+        <router-link class="nav-link" :to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
+        <router-link class="nav-link" :to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      </nav>
+    </header>
     <router-view />
   </div>
 </template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Luckiest+Guy&family=McLaren&display=swap');
+</style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+
   </div>
 </template>
 
 <script>
+
 export default {
+  components: {
+
+  }
 };
 </script>
