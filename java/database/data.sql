316eb9be8d85900e80696eb5400fba0de972c329
Nick Hamm <nick@deadmanworking.io>
2024-04-05 15:01:52 -0400

    Update schema.sql for required fields; update data.sql for new fields

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,6 +1,8 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+INSERT INTO users (username,password_hash,role, first_name, last_name, birthdate)
+VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'Steve', 'McQueen', '1930-03-24');
+INSERT INTO users (username,password_hash,role, first_name, last_name, birthdate)
+VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'Kae', 'Bonaguro', '1996-05-02');
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -7,6 +7,9 @@ CREATE TABLE users (
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
+    first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    birthdate date NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
@@ -14,6 +17,10 @@ CREATE TABLE pets(
     pet_id SERIAL,
     name varchar(50) NOT NULL,
     owner_id int NOT NULL,
+    age int NOT NULL,
+    breed varchar(25) NOT NULL,
+    size varchar(10) NOT NULL,
+    isFriendly boolean NOT NULL,
     CONSTRAINT PK_pet PRIMARY KEY (pet_id),
     CONSTRAINT FK_pet_owner FOREIGN KEY (owner_id) REFERENCES users (user_id)
 );
