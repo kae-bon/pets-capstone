b4ca1fd877e8adf1e9dec22f4dd6cfa581121120
Malik Smith <m.smith1017.whhs@gmail.com>
2024-04-07 22:25:30 -0400

    added styling to nav and refactored component names

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,10 +4,11 @@
       <nav id="nav" class="navbar justify-content-between">
         <img class="navbar-brand" src="@/assets/nav-logo.png" alt="woofr">
         <div class="router-links">
-          <router-link class="nav-link" :to="{ name: 'woofr' }">Home</router-link>
+          <router-link class="nav-link" :to="{ name: 'woofr' }" v-if="$store.state.token == ''">Home</router-link>
+          <router-link class="nav-link" :to="{ name: 'user-home' }" v-else>Home</router-link>
           <router-link class="nav-link" :to="{ name: 'register' }" v-if="$store.state.token == ''">Register</router-link>
           <router-link class="nav-link" :to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
-          <router-link class="nav-link" :to="{ name: 'woofr' }" v-if="$store.state.token != ''">Logout</router-link>
+          <router-link class="nav-link" :to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
         </div>
       </nav>
     </header>
