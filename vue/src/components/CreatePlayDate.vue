5250afd7f5fb402b86dabcd99dba201cb2affdcd
Kae Bonaguro <k.n.bonaguro@gmail.com>
2024-04-08 16:21:06 -0400

    Added modal styling for new playdate form, and bound form inputs to new play date object

--- vue/src/components/CreatePlayDate.vue
+++ vue/src/components/CreatePlayDate.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
                     <h1 class="modal-title fs-5" id="playDateModal">Create Play Date</h1>
@@ -9,19 +9,24 @@
                 <div class="modal-body">
                     <form>
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="title" placeholder="title">
+                            <input type="text" class="form-control" id="title" placeholder="title" maxlength="100"
+                                v-model="newPlayDate.title">
                             <label for="title">Title</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="description" placeholder="description">
+                            <textarea class="form-control" id="description" placeholder="description" maxlength="300"
+                                v-model="newPlayDate.description">
+                                </textarea>
                             <label for="description">Description</label>
                         </div>
                         <div class="form-floating mb-3">
-                            <input type="datetime-local" class="form-control" id="date" placeholder="date">
+                            <input type="datetime-local" class="form-control" id="date" placeholder="date"
+                                v-model="newPlayDate.dateTime">
                             <label for="date">Date & Time</label>
                         </div>
-                        <div class="form-floating">
-                            <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                        <div class="form-floating mb-3">
+                            <select class="form-select" id="floatingSelect" aria-label="Floating label select example"
+                                v-model="newPlayDate.location">
                                 <option selected>---</option>
                                 <option value="1">placeholder</option>
                                 <option value="2">placeholder2</option>
@@ -30,24 +35,24 @@
                             <label for="floatingSelect">Select a Location</label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
-                                checked>
-                            <label class="form-check-label" for="flexRadioDefault1">
-                                Public
+                            <input class="form-check-input" type="radio" value="true" name="flexRadioDefault"
+                                id="radiopublic" v-model="newPlayDate.isPublic" checked>
+                            <label class="form-check-label" for="radiopublic">
+                                Public - anyone can join!
                             </label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"
-                                checked>
-                            <label class="form-check-label" for="flexRadioDefault1">
-                                Private
+                            <input class="form-check-input" type="radio" value="false" name="flexRadioDefault"
+                                id="radioprivate" v-model="newPlayDate.isPublic">
+                            <label class="form-check-label" for="radioprivate">
+                                Private - you approve who comes!
                             </label>
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Create Play Date</button>
                 </div>
             </div>
         </div>
@@ -56,8 +61,26 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            newPlayDate: {
+                title: "",
+                description: "",
+                dateTime: "",
+                location: "",
+                isPublic: true
+            }
+        }
+    }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.modal-footer {
+    justify-content: space-between;
+}
+
+#description {
+    height: 175px;
+}
+</style>
--- vue/src/views/UserHomeView.vue
+++ vue/src/views/UserHomeView.vue
@@ -1,13 +1,36 @@
 <template>
     <div>
+        <div>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                <i class="fa-solid fa-dog" style="color: #2ab7ca;"></i>
+            </button>
+            <p>Click the Dog to Register a new Play Date!</p>
 
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <CreatePlayDate class="createPlayDate" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import CreatePlayDate from "../components/CreatePlayDate.vue";
+
 export default {
+    components: {
+        CreatePlayDate
+    }
 
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+i {
+    font-size: 5rem;
+}
+
+.createPlayDate {
+    height: 100%;
+}
+</style>
