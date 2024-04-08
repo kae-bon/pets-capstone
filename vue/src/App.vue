ee00e5d0080bce882b5cb01916ba78857ead068c
Kae Bonaguro <k.n.bonaguro@gmail.com>
2024-04-08 09:46:40 -0400

    Changed semantics of login vs register new user

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -8,7 +8,7 @@
         <div class="router-links">
           <router-link class="nav-link" :to="{ name: 'home' }">Home</router-link>
           <router-link class="nav-link" :to="{ name: 'register' }" v-if="$store.state.token == ''">Register</router-link>
-          <router-link class="nav-link" :to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
+          <router-link class="nav-link" :to="{ name: 'login' }" v-if="$store.state.token == ''">Sign In</router-link>
           <router-link class="nav-link" :to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
         </div>
       </nav>
