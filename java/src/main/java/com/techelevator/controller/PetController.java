5256345973041738cc0568db4cb503e1e09d8fa0
Nick Hamm <nick@deadmanworking.io>
2024-04-08 17:07:10 -0400

    Begin work to validate pet is not in database in PetController.java

--- java/src/main/java/com/techelevator/controller/PetController.java
+++ java/src/main/java/com/techelevator/controller/PetController.java
@@ -20,10 +20,18 @@ public class PetController {
 
     private PetDao petDao;
 
+    public PetController(PetDao petDao) {
+        this.petDao = petDao;
+    }
+
     @RequestMapping(path = "/register", method = RequestMethod.POST)
     public void register(@Valid @RequestBody RegisterPetDto newPet) {
         try {
             if (petDao.getPetByPetname(newPet.getPetName()) != null) {
+                //TODO:
+                // 1. Get pets associated with owner
+                // 2. Verify that pet doesn't already exist
+                // 3. Uniqueness is defined by pet.name + pet.birthdate + pet.breed + pet.ownerId
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "PetName already exists.");
             } else {
                 petDao.createPet(newPet);
