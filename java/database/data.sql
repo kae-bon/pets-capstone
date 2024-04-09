2f348636d82a3ede25c0d04b459ccc22c477f7d8
Grethel Huerta <grethel.hrta@gmail.com>
2024-04-09 14:41:01 -0400

    created owner table sql

--- java/database/data.sql
+++ java/database/data.sql
@@ -1,11 +1,21 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role, first_name, last_name, birthdate)
-VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER', 'Steve', 'McQueen', '1930-03-24');
-INSERT INTO users (username,password_hash,role, first_name, last_name, birthdate)
-VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN', 'Kae', 'Bonaguro', '1996-05-02');
+INSERT INTO users (username,password_hash,role)
+VALUES ('stevemcqueeniscool@yahoo.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER');
+
+INSERT INTO users (username,password_hash,role)
+VALUES ('KaeisCooler@gmail.com','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN');
+
+INSERT INTO owners (first_name, last_name, birthdate, email)
+VALUES ('Steve', 'McQueen', '1930-03-24', 'stevemcqueeniscool@yahoo.com');
+
+INSERT INTO owners (first_name, last_name, birthdate, email)
+VALUES ('Kae', 'Bonaguro', '1996-05-02', 'KaeisCooler@gmail.com');
 
 INSERT INTO pets (name, owner_id, birthdate, breed, size, isFriendly)
-VALUES ('Bean', '1','1996-05-02','pug', 'small', 'true');
+VALUES ('Bean', '1','1996-05-02','pug', 'S', 'true');
+
+INSERT INTO pets (name, owner_id, birthdate, breed, size, isFriendly)
+VALUES ('Steve', '2', '2008-03-05', 'labrador', 'XL', 'true');
 
 COMMIT TRANSACTION;
--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,17 +1,25 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, pets, owners;
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
-    first_name varchar(50) NOT NULL,
-    last_name varchar(50) NOT NULL,
-    birthdate date NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+ CREATE TABLE owners(
+ 	owner_id SERIAL, 
+	first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    birthdate date NOT NULL,
+	email varchar(50) NOT NULL UNIQUE, 
+	CONSTRAINT PK_owner PRIMARY KEY (owner_id),
+	CONSTRAINT FK_user_owner FOREIGN KEY (email) REFERENCES users (username)
+	 	 
+ );
 
 CREATE TABLE pets(
     pet_id SERIAL,
@@ -22,8 +30,10 @@ CREATE TABLE pets(
     size varchar(10) NOT NULL,
     isFriendly boolean NOT NULL,
     CONSTRAINT PK_pet PRIMARY KEY (pet_id),
-    CONSTRAINT FK_pet_owner FOREIGN KEY (owner_id) REFERENCES users (user_id),
+    CONSTRAINT FK_pet_owner FOREIGN KEY (owner_id) REFERENCES owners (owner_id),
     CONSTRAINT unique_pet_details UNIQUE (owner_id, name, breed, birthdate)
 );
+
+
 
 COMMIT TRANSACTION;
