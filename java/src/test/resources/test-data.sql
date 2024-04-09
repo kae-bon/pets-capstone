a7af86ed5f18edbafcdb9c26bc0cde73779407ee
Kae Bonaguro <k.n.bonaguro@gmail.com>
2024-04-09 19:46:57 -0400

    Inserted location test data and wrote Jdbc Location tests for getting all locations

--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -8,4 +8,8 @@ INSERT INTO play_dates(title, description, host_id, date_time, location_setting,
 VALUES ('Test', 'Testy Tests', 1, '2024-05-02 14:30:00', 'Grant Park', true),
 ('Luna', 'With Luna', 1, '2024-04-15 12:00:00', 'Washington Park', false);
 
+INSERT INTO locations(location_name, street_address, city, state_abbr)
+VALUES ('Otto Armleder Dog Park', '5057 Wooster Pike', 'Cincinnati', 'OH'),
+('Fido Field', '630 Eggleston Ave', 'Cincinnati', 'OH');
+
 COMMIT TRANSACTION;
