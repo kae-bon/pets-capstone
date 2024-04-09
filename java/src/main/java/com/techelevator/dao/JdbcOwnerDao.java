4bede588d12b181d6e5e5632cc4f41e9203c34ac
Grethel Huerta <grethel.hrta@gmail.com>
2024-04-09 16:00:25 -0400

    owner java files

---
+++ java/src/main/java/com/techelevator/dao/JdbcOwnerDao.java
@@ -0,0 +1,67 @@
+package com.techelevator.dao;
+
+import com.techelevator.exception.DaoException;
+import com.techelevator.exception.DuplicatePetException;
+import com.techelevator.model.Owner;
+import com.techelevator.model.RegisterOwnerDto;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JdbcOwnerDao implements OwnerDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcOwnerDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    private final String SELECT_SQL = "SELECT owner_id, first_name, last_name, birthdate, email FROM owners ";
+
+
+    @Override
+    public Owner createOwner(RegisterOwnerDto owner) {
+        Owner newOwner = null;
+        String sql = "INSERT INTO owners(first_name, last_name, birthdate, email) values(?, ?, ?, ?) RETURNING owner_id;";
+        try {
+            int newOwnerId = jdbcTemplate.queryForObject(
+                    sql,
+                    int.class, owner.getFirstName(),
+                    owner.getLastName(),
+                    owner.getBirthdate(),
+                    owner.getEmail());
+            newOwner = getNewOwnerById(newOwnerId);
+        }  catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to server or database", e);
+        } catch (DataAccessException e) {
+            throw new DaoException("Data integrity violation", e);
+        }
+        return newOwner;
+    }
+
+    private Owner getNewOwnerById(int newOwnerId) {
+        Owner owner = null;
+        String sql = SELECT_SQL + "WHERE owner_id = ?;";
+        try {
+            SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, id);
+            while (rowSet.next()) {
+                owner = mapRowToOwner(rowSet);
+            }
+        } catch (CannotGetJdbcConnectionException e) {
+            throw new DaoException("Unable to connect to the database", e);
+        }
+        return owner;
+        }
+
+    }
+
+
+}
+
+    private Owner mapRowToOwner(SqlRowSet rowSet) {
+    }
+
--- java/src/main/java/com/techelevator/model/RegisterOwnerDto.java
+++ java/src/main/java/com/techelevator/model/RegisterOwnerDto.java
@@ -40,4 +40,8 @@ public class RegisterOwnerDto {
 
     public RegisterOwnerDto() {
     }
+
+    public String getEmail() {
+    }
 }
+
