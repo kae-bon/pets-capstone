5d2fdd7d38813706e1254322b80e545407da35c6
Malik Smith <m.smith1017.whhs@gmail.com>
2024-04-09 12:19:25 -0400

    added hostId to playdatecontroller and hooked frontend to backend. also added basic notifications

--- java/src/main/java/com/techelevator/controller/PlayDateController.java
+++ java/src/main/java/com/techelevator/controller/PlayDateController.java
@@ -1,31 +1,36 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.PlayDateDao;
+import com.techelevator.dao.UserDao;
 import com.techelevator.exception.DaoException;
 import com.techelevator.model.PlayDate;
+import com.techelevator.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
 @RestController
 @PreAuthorize("isAuthenticated()")
+@CrossOrigin
 public class PlayDateController {
     private PlayDateDao playDateDao;
-    public PlayDateController(PlayDateDao playDateDao) {
+    private UserDao userDao;
+    public PlayDateController(PlayDateDao playDateDao, UserDao userDao) {
         this.playDateDao = playDateDao;
+        this.userDao = userDao;
     }
 
     // TODO: need to add Pet play date connection
     @PostMapping("/playdates")
     @ResponseStatus(HttpStatus.CREATED)
-    public PlayDate createPlayDate(@RequestBody PlayDate newPlayDate) {
+    public PlayDate createPlayDate(@RequestBody PlayDate newPlayDate, Principal user) {
         try {
+            User owner = userDao.getUserByUsername(user.getName());
+            int userId = owner.getId();
+            newPlayDate.setHostId(userId);
             return playDateDao.createPlayDate(newPlayDate);
         } catch (DaoException e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Playdate registration failed.");
--- vue/src/App.vue
+++ vue/src/App.vue
@@ -17,12 +17,39 @@
         </div>
       </nav>
     </header>
+
+    <div v-bind:class="notificationClass" v-show="notification" v-on:click="clearNotification">
+      {{ notification?.message }}
+    </div>
+
     <router-view />
     <footer class="d-flex align-items-center justify-content-center p-2 mt-5">
-      <p>&copy Luna & Marley Co.</p>
+      <p>&copy; Luna & Marley Co.</p>
     </footer>
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    notification() {
+      return this.$store.state.notification;
+    },
+    notificationClass() {
+      return {
+        'status-message': true,
+        error: this.notification?.type?.toLowerCase() === 'error',
+        success: this.notification?.type?.toLowerCase() === 'success'
+      };
+    }
+  },
+  methods: {
+    clearNotification() {
+      this.$store.commit('CLEAR_NOTIFICATION');
+    }
+  }
+}
+</script>
 
 <style scoped>
 img {
