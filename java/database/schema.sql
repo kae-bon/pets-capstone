a66af609ef050ab0b0f5de373759fbc5c76dbc1b
Nick Hamm <nick@deadmanworking.io>
2024-04-10 09:58:16 -0400

    Added changes to database from create-account story

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, pets, owners;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -9,5 +9,31 @@ CREATE TABLE users (
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
+
+ CREATE TABLE owners(
+ 	owner_id SERIAL, 
+	first_name varchar(50) NOT NULL,
+    last_name varchar(50) NOT NULL,
+    birthdate date NOT NULL,
+	email varchar(50) NOT NULL UNIQUE, 
+	CONSTRAINT PK_owner PRIMARY KEY (owner_id),
+	CONSTRAINT FK_user_owner FOREIGN KEY (email) REFERENCES users (username)
+	 	 
+ );
+
+CREATE TABLE pets(
+    pet_id SERIAL,
+    name varchar(50) NOT NULL,
+    owner_id int NOT NULL,
+    birthdate date NOT NULL,
+    breed varchar(25) NOT NULL,
+    size varchar(10) NOT NULL,
+    isFriendly boolean NOT NULL,
+    CONSTRAINT PK_pet PRIMARY KEY (pet_id),
+    CONSTRAINT FK_pet_owner FOREIGN KEY (owner_id) REFERENCES owners (owner_id),
+    CONSTRAINT unique_pet_details UNIQUE (owner_id, name, breed, birthdate)
+);
+
+
 
 COMMIT TRANSACTION;
--- java/src/test/resources/test-data.sql
+++ java/src/test/resources/test-data.sql
@@ -1,7 +1,29 @@
 BEGIN TRANSACTION;
 
-INSERT INTO users (username,password_hash,role) VALUES ('user1','user1','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('user2','user2','ROLE_USER');
-INSERT INTO users (username,password_hash,role) VALUES ('user3','user3','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('stevemcqueeniscool@yahoo.com','user1','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('KaeisCooler@gmail.com','user2','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('nickisold@aol.com','user3','ROLE_USER');
+INSERT INTO users (username,password_hash,role) VALUES ('carlyiscool@gmail.com', 'user4', 'ROLE_USER');
 
-COMMIT TRANSACTION;
+INSERT INTO owners (first_name, last_name, birthdate, email)
+VALUES ('Steve', 'McQueen', '1930-03-24', 'stevemcqueeniscool@yahoo.com');
+
+INSERT INTO owners (first_name, last_name, birthdate, email)
+VALUES ('Kae', 'Bonaguro', '1996-05-02', 'KaeisCooler@gmail.com');
+
+INSERT INTO owners (first_name, last_name, birthdate, email)
+VALUES ('Nick', 'Hamm', '1981-09-18', 'nickisold@aol.com');
+
+INSERT INTO pets (name, owner_id, birthdate, breed, size, isFriendly)
+VALUES ('Bean', '1','1996-05-02','pug', 'S', 'true');
+
+INSERT INTO pets (name, owner_id, birthdate, breed, size, isFriendly)
+VALUES ('Steve', '2', '2008-03-05', 'labrador', 'XL', 'true');
+
+
+
+
+
+
+
+ COMMIT TRANSACTION;
